7e770719799d9c15879a4fb1f78d7fba53f7c83d
Filiakov Oleksii <oleksii.filiakov@tptlive.ee>
2024-11-10 20:45:02 +0200

    explain es vs commonjs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import common from "./common.js";
-
 common.hello();
-
 import es from "./es.js";
-
 es.hello();
+
+// const common require = require("./common.js");
+// common.hello();
+// const es require = require("./es.js");
+// es.hello();
