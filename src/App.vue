fbc56b5c7d7adbfc89f5b3ae22ff4d0b6ecbe35f
Oleksii Filiakov <oleksii.filiakov@tptlive.ee>
2024-12-02 11:40:52 +0200

    add vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+let message = 'Hello vue!'
+</script>
+<template>
+    <div class="container">
+        <div class="content">
+            <h1>{{ message }}</h1>
+        </div>
+    </div>
+</template>
+<style></style>
