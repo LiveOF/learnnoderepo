0b51c80633a09e6498c33b21ba2dfae64025a1cc
Oleksii Filiakov <oleksii.filiakov@tptlive.ee>
2024-12-09 10:23:09 +0200

    setTimeout example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup>
-let message = 'Hello vue!'
+import { ref } from 'vue';
+
+let message = ref('Hello vue!')
+setTimeout(() => {
+    message.value = "Hello";
+    console.log('Msg changed');
+}, 10_000);
 </script>
 <template>
     <div class="container">
