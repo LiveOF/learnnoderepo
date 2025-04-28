f88baad230a7e453b860ca11694ef75befde15af
Oleksii Filiakov <oleksii.filiakov@tptlive.ee>
2025-04-28 20:55:00 +0300

    Add websocket example

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -5,35 +5,49 @@ import { ref } from 'vue';
 let message = ref('');
 let messages = ref([]);
 
-let res = await axios.get('http://localhost:3000/messages');
-messages.value.push(...res.data);
 
-const eventSource = new EventSource('http://localhost:3000/messages/sse');
- 
- eventSource.addEventListener('newmessage', (event) => {
-     console.log(event);
-     let data = JSON.parse(event.data);
-     messages.value.push(...data);
- });
+// let res = await axios.get('http://localhost:3000/messages');
+// messages.value.push(...res.data);
 
- async function send() {
-     let res = await axios.post('http://localhost:3000/messages', {
-         message: message.value
-     });
-     message.value = '';
- }
- </script>
- 
+// Create WebSocket connection.
+const socket = new WebSocket("ws://localhost:8080");
+
+// Connection opened
+socket.addEventListener("open", (event) => {
+  //socket.send("Hello Server!");
+});
+
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  
+  let data = JSON.parse(event.data);
+  console.log(data);
+  if(data.type === 'messages') {
+    messages.value.push(...data.messages);
+  }
+  if(data.type === 'message') {
+    messages.value.push(data);
+  }
+});
+
+
+
+async function send() {
+    socket.send(JSON.stringify({type: 'message', message: message.value}));
+    message.value = '';
+}
+</script>
+
 <template>
-     <div class="field has-addons">
-         <div class="control is-expanded">
-             <input class="input" type="text" v-model="message" @keypress.enter="send">
-         </div>
-         <div class="control">
-             <button class="button is-primary" @click="send">Send</button>
-         </div>
-     </div>
-     <div class="notification is-primary" v-for="msg in messages">
-         {{ msg.message }}        
-     </div>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input class="input" type="text" v-model="message" @keypress.enter="send">
+        </div>
+        <div class="control">
+            <button class="button is-primary" @click="send">Send</button>
+        </div>
+    </div>
+    <div class="notification is-primary" v-for="msg in messages">
+        {{ msg.message }}        
+    </div>
 </template>
