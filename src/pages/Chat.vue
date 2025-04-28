ab14b6d4ff94f899b2409201f48e5fb3e6fd9106
Oleksii Filiakov <oleksii.filiakov@tptlive.ee>
2025-04-28 20:57:23 +0300

    Add sse example

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -4,52 +4,36 @@ import { ref } from 'vue';
 
 let message = ref('');
 let messages = ref([]);
-let lastMessageDate = null;
 
 let res = await axios.get('http://localhost:3000/messages');
 messages.value.push(...res.data);
-if(res.data.length > 0){
-    let lastMessage = res.data[res.data.length-1];
-    lastMessageDate = lastMessage.date;
-}
 
-longPoll();
+const eventSource = new EventSource('http://localhost:3000/messages/sse');
+ 
+ eventSource.addEventListener('newmessage', (event) => {
+     console.log(event);
+     let data = JSON.parse(event.data);
+     messages.value.push(...data);
+ });
 
-async function longPoll(){
-    let res = await axios.get('http://localhost:3000/messages/longpoll', {
-        params: {
-            date: lastMessageDate
-        }
-    });
-    messages.value.push(...res.data);
-    if(res.data.length > 0){
-        let lastMessage = res.data[res.data.length-1];
-        lastMessageDate = lastMessage.date;
-    }
-    await longPoll();
-}
-
-
-
-
-async function send() {
-    let res = await axios.post('http://localhost:3000/messages', {
-        message: message.value
-    });
-    message.value = '';
-}
-</script>
-
+ async function send() {
+     let res = await axios.post('http://localhost:3000/messages', {
+         message: message.value
+     });
+     message.value = '';
+ }
+ </script>
+ 
 <template>
-    <div class="field has-addons">
-        <div class="control is-expanded">
-            <input class="input" type="text" v-model="message" @keypress.enter="send">
-        </div>
-        <div class="control">
-            <button class="button is-primary" @click="send">Send</button>
-        </div>
-    </div>
-    <div class="notification is-primary" v-for="msg in messages">
-        {{ msg.message }}        
-    </div>
+     <div class="field has-addons">
+         <div class="control is-expanded">
+             <input class="input" type="text" v-model="message" @keypress.enter="send">
+         </div>
+         <div class="control">
+             <button class="button is-primary" @click="send">Send</button>
+         </div>
+     </div>
+     <div class="notification is-primary" v-for="msg in messages">
+         {{ msg.message }}        
+     </div>
 </template>
