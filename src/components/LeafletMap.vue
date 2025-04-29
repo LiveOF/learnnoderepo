33dae71b7c6a337cc1b63093271d241d5221bf21
Oleksii Filiakov <oleksii.filiakov@tptlive.ee>
2025-04-29 11:33:54 +0300

    adding home location

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,12 +1,68 @@
 <script setup>
  import L from 'leaflet';
  import 'leaflet/dist/leaflet.css';
- import { onMounted, useId, watch } from 'vue';
+ import { onMounted, useId, watch, ref } from 'vue';
  
- let { center, zoom } = defineProps(['center', 'zoom']);
+ // Fix Leaflet's default icon path problem
+ delete L.Icon.Default.prototype._getIconUrl;
+ L.Icon.Default.mergeOptions({
+   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+   shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png'
+ });
+ 
+ let { center, zoom, marker, polygon } = defineProps({
+   center: {
+     type: Array,
+     required: true
+   },
+   zoom: {
+     type: Number,
+     required: true
+   },
+   marker: {
+     type: Array,
+     default: null
+   },
+   polygon: {
+     type: Array,
+     default: () => []
+   }
+ });
  
  let id = 'map-' + useId();
  let map;
+ let markerInstance = ref(null);
+ let polygonInstance = ref(null);
+
+ // Function to add or update marker
+ const updateMarker = (coords) => {
+   if (!map) return;
+   
+   // Remove existing marker if it exists
+   if (markerInstance.value) {
+     map.removeLayer(markerInstance.value);
+   }
+   
+   // Add new marker if coordinates provided
+   if (coords) {
+     markerInstance.value = L.marker(coords).addTo(map);
+   }
+ };
+ 
+ // Function to add or update polygon
+ const updatePolygon = (points) => {
+   if (!map || !points || points.length < 3) return;
+   
+   // Remove existing polygon if it exists
+   if (polygonInstance.value) {
+     map.removeLayer(polygonInstance.value);
+   }
+   
+   // Add new polygon
+   polygonInstance.value = L.polygon(points, { color: 'red' }).addTo(map);
+ };
+
  onMounted(() => {
      console.log(document.getElementById(id));
  
@@ -15,16 +71,32 @@
          maxZoom: 19,
          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
      }).addTo(map);
+     
+     // Initialize marker and polygon if provided
+     if (marker) updateMarker(marker);
+     if (polygon && polygon.length > 0) updatePolygon(polygon);
  });
  
  watch(() => center, (newCenter, oldCenter) => {
      console.log(newCenter, oldCenter);
      map.panTo(newCenter);
  });
+ 
  watch(() => zoom, newZoom => {
      map.setZoom(newZoom);
  });
+ 
+ // Watch for marker changes
+ watch(() => marker, (newMarker) => {
+     updateMarker(newMarker);
+ });
+ 
+ // Watch for polygon changes
+ watch(() => polygon, (newPolygon) => {
+     updatePolygon(newPolygon);
+ }, { deep: true });
  </script>
+ 
  <template>
       <div :id="id"></div>
  </template>
--- src/pages/Leaflet.vue
+++ src/pages/Leaflet.vue
@@ -1,13 +1,27 @@
 <script setup>
- import { ref } from 'vue';
- import LeafletMap from '../components/LeafletMap.vue';
+import { ref } from 'vue';
+import LeafletMap from '../components/LeafletMap.vue';
  
- let coords = ref([59.42691, 24.74344]);
- let zoom = ref(19);
- </script>
- <template>
-     <button class="button is-primary" @click="coords=[58.378003935892494, 26.727245723399832]">Go to Tartu</button>
-     <input type="range" min="1" max="19" step="1" v-model="zoom">
-     <LeafletMap :center="coords" :zoom="zoom"></LeafletMap>
-     <LeafletMap :center="[59.4332429007378, 24.75129553356808]" :zoom="10"></LeafletMap>
- </template>
+let coords = ref([59.42691, 24.74344]); // Initial map center
+let zoom = ref(19);
+let homeCoords = [59.441424, 24.738884];
+let homePolygon = [
+  // Updated coordinates to make a smaller polygon just around house #31
+  [59.441458, 24.738535], // Top-left corner
+  [59.441683, 24.739414], // Top-right corner
+  [59.441524, 24.739579], // Bottom-right corner
+  [59.441253, 24.738623], // Bottom-left corner
+  [59.441458, 24.738535]  // Back to first point to close polygon
+];
+</script>
+
+<template>
+    <button class="button is-primary" @click="coords=homeCoords">Go to my home</button>
+    <input type="range" min="1" max="19" step="1" v-model="zoom">
+    <LeafletMap 
+        :center="coords" 
+        :zoom="zoom"
+        :marker="homeCoords"
+        :polygon="homePolygon"
+    ></LeafletMap>
+</template>
